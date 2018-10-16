39b5383eb9c2682f498583f7c95f0e6c844a8560
chintan prajapati <chintanprajapati37@gmail.com>
2018-10-16 16:05:32 +0530

    add dotenv

--- src/config.js
+++ src/config.js
@@ -1,23 +1,10 @@
 /**
  *  Application configuration
  */
-export const  RESOURCE = 'app';
-export const  APP_MODE = 'DEV'; // DEV , PROD
-export const  APP_VERSION = '1.9.5-rc.5';
-
-let appUrl;
-let chatUrl;
-let appSecret;
-
-if(APP_MODE == 'PROD'){
-    appUrl = 'https://mkeys.flashcoin.io';
-    chatUrl = 'https://chat.flashcoin.io';
-    appSecret = '12p]l+Or^}ZT8m_p[qC~';
-} else{
-    appUrl = 'https://dev03keys.flashcoin.io';
-    chatUrl = 'https://chatdev.flashcoin.io';
-    appSecret = 'p]l+Or^}ZT8m_p[qC~';
-}
+import { appMode, appVersion, chatUrl, appUrl, appSecret } from 'react-native-dotenv'
+export const RESOURCE = 'app';
+export const APP_MODE = appMode; // DEV , PROD
+export const APP_VERSION = appVersion;
 export const API_URL = appUrl+'/api';
 export const PROFILE_URL = appUrl+'/profile/';
 export const CHAT_URL = chatUrl;
